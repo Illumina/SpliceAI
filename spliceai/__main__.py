3bca85e1011c2a4ff9c6d4b5791e3dbad6e80af8
Jeremy McRae <jmcrae@illumina.com>
2018-11-08 11:08:34 -0800

    allow for reading from standard input and write to standard ouput

--- spliceai/__main__.py
+++ spliceai/__main__.py
@@ -1,19 +1,37 @@
 import argparse
+import sys
 import vcf
 from spliceai.utils import annotator, get_delta_scores
 
-
-def main():
-
+def get_options():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-I')
-    parser.add_argument('-O')
-    parser.add_argument('-R')
-    parser.add_argument('-A')
+    parser.add_argument('vcf', nargs='?', default=sys.stdin.buffer,
+        help='path to VCF input, defaults to standard in')
+    parser.add_argument('out', nargs='?', default=sys.stdout,
+        help='path to write output VCF to, defaults to standard out.')
+    parser.add_argument('-R', required=True,
+        help='path to genome fasta file (must be fai indexed for quick access)')
+    parser.add_argument('-A',
+        help='path to gencode genes file (defaults to file in package)')
     args = parser.parse_args()
 
-    vcf_reader = vcf.Reader(open(args.I, 'r'))
-    vcf_writer = vcf.Writer(open(args.O, 'w'), vcf_reader)
+    try:
+        args.vcf = open(args.vcf, 'rt')
+    except TypeError:
+        pass
+
+    try:
+        args.out = open(args.out, 'wt')
+    except TypeError:
+        pass
+
+    return args
+
+def main():
+    args = get_options()
+
+    vcf_reader = vcf.Reader(args.vcf)
+    vcf_writer = vcf.Writer(args.out, vcf_reader)
     ann = annotator(args.R, args.A)
 
     for record in vcf_reader:
