7a0f8172e199ac82b58842fad761ce4882ab04b3
Jeremy McRae <jmcrae@illumina.com>
2018-11-13 14:47:01 -0800

    swapped input and output variable names back

--- spliceai/__main__.py
+++ spliceai/__main__.py
@@ -5,9 +5,9 @@ from spliceai.utils import annotator, get_delta_scores
 
 def get_options():
     parser = argparse.ArgumentParser()
-    parser.add_argument('vcf', nargs='?', default=sys.stdin.buffer,
+    parser.add_argument('I', nargs='?', default=sys.stdin.buffer,
         help='path to VCF input, defaults to standard in')
-    parser.add_argument('out', nargs='?', default=sys.stdout,
+    parser.add_argument('O', nargs='?', default=sys.stdout,
         help='path to write output VCF to, defaults to standard out.')
     parser.add_argument('-R', required=True,
         help='path to genome fasta file (must be fai indexed for quick access)')
@@ -16,12 +16,12 @@ def get_options():
     args = parser.parse_args()
 
     try:
-        args.vcf = open(args.vcf, 'rt')
+        args.I = open(args.I, 'rt')
     except TypeError:
         pass
 
     try:
-        args.out = open(args.out, 'wt')
+        args.O = open(args.O, 'wt')
     except TypeError:
         pass
 
@@ -30,10 +30,10 @@ def get_options():
 def main():
     args = get_options()
 
-    vcf = pysam.VariantFile(args.vcf)
+    vcf = pysam.VariantFile(args.I)
     header = vcf.header
     header.add_line('##INFO=<ID=SpliceAI,Number=.,Type=String,Description="Splice AI annotation for variant. These include delta scores (DS) for acceptor gain (AG), acceptor loss (AL), donor gain (DG) and donor loss (DL). The distance from the variant site to the splice site is also included (DP). Format: Allele|SYMBOL|DS_AG|DS_AL|DS_DG|DS_DL|DP_AG|DP_AL|DP_DG|DP_DL">')
-    output = pysam.VariantFile(args.out, mode='w', header=header)
+    output = pysam.VariantFile(args.O, mode='w', header=header)
     ann = annotator(args.R, args.A)
 
     for record in vcf:
@@ -45,4 +45,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
